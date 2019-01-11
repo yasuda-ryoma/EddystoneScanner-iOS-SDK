1d0c61b89fed74a6cae3e6e508b14566b4fee11a
Takeshi Yokemura <yokemuratakeshi@team-lab.com>
2019-01-11 11:24:48 +0900

    Byte array to Int utility

--- EddystoneScanner/EddystoneRFC/Util.swift
+++ EddystoneScanner/EddystoneRFC/Util.swift
@@ -21,9 +21,26 @@ public extension Collection where Iterator.Element == UInt8 {
     /// Converts the UInt8 array into its quvivalent hex string
     ///
     public var hexString: String {
-        return map{ String(format: "%02X", $0) }.joined()
+        return map { String(format: "%02X", $0) }.joined()
     }
     
+    ///
+    /// Converts the UInt8 array into system-dependent unsigned int
+    ///
+    public var UIntValue: UInt {
+        return bytesToAnyUInt(byteArray: ArraySlice(self)) ?? 0
+    }
+    
+    /// Converts the UInt8 array into bytes-specified unsigned int
+    ///
+    ///// 64bit
+    public var UInt64Value: UInt64 {
+        return bytesToAnyUInt(byteArray: ArraySlice(self)) ?? 0
+    }
+    ///// 32bit
+    public var UInt32Value: UInt32 {
+        return bytesToAnyUInt(byteArray: ArraySlice(self)) ?? 0
+    }
 }
 
 /**
@@ -31,16 +48,22 @@ public extension Collection where Iterator.Element == UInt8 {
  
  - Parameter byteArray: The `ArraySlice<UInt8>` byte array
  */
+// Specificly for 'UInt' type (for compatibility to existing code)
 internal func bytesToUInt(byteArray: ArraySlice<UInt8>) -> UInt? {
-    guard byteArray.count <= 4 else {
-        debugPrint("Byte array cannot be converted into a UInt object. Elements greater that 4")
+    return bytesToAnyUInt(byteArray: byteArray)
+}
+
+internal func bytesToAnyUInt<T: UnsignedInteger>(byteArray: ArraySlice<UInt8>) -> T? {
+    let maxCount = MemoryLayout<T>.size / MemoryLayout<UInt8>.size
+    guard byteArray.count <= maxCount else {
+        debugPrint("Byte array cannot be converted into a specified integer object. Elements greater than \(maxCount)")
         return nil
     }
     
-    var result: UInt = 0
+    var result: T = 0
     for idx in 0..<(byteArray.count) {
-        let shiftAmount = UInt((byteArray.count) - idx - 1) * 8
-        let value = UInt(byteArray[byteArray.startIndex + idx])
+        let shiftAmount = T((byteArray.count) - idx - 1) * 8
+        let value = T(byteArray[byteArray.startIndex + idx])
         result += value << shiftAmount
     }
     return result
